b0694c04019b2009c168b4bbb77242654d8f8784
Conrad Buck <conartist6@gmail.com>
2025-08-21 09:20:44 -0600

    parse returns unwrapped nodes

--- test/grammar.test.js
+++ test/grammar.test.js
@@ -15,7 +15,7 @@ const buildJSONTag = (matcher) => {
 };
 
 const print = (tree) => {
-  return printPrettyCSTML(tree.node);
+  return printPrettyCSTML(tree);
 };
 
 describe('@bablr/language-en-json', () => {
